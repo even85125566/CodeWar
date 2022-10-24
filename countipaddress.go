ee9e80fe8c3f98f9cb3188f0766ee856d42fba15
鍾亦凡 <even85125566@gmail.com>
2022-10-24 17:19:47 +0800

    countipaddress

---
+++ countipaddress.go
@@ -0,0 +1,40 @@
+package codewar
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func IpsBetween(start, end string) int {
+	return int(IPTest(start, end))
+
+}
+func IPTest(start string, end string) (number float64) {
+	startString := strings.Split(start, ".")
+	endString := strings.Split(end, ".")
+
+	startIP := getNumber(startString)
+	endIP := getNumber(endString)
+	for i := 0; i < 4; i++ {
+
+		number += math.Pow(float64(256), float64(3-i)) * float64(endIP[i]-startIP[i])
+
+	}
+	return
+
+}
+func getNumber(stringSlice []string) (numberslice []int) {
+	for _, eachstring := range stringSlice {
+		eachNumber, err := strconv.Atoi(eachstring)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		numberslice = append(numberslice, eachNumber)
+	}
+
+	return
+
+}
